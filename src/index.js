a122fffc121f37b080cd4abba617d1d231f5b2fd
Neida <neida@duck.com>
2023-01-27 13:55:46 -0500

    initialized project

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from './App';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+import { routes } from "./Router";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter(routes);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
