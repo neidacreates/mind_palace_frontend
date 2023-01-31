3347518f5c27b0092b407785c1822de0b4d124e9
Neida <neida@duck.com>
2023-01-30 21:47:14 -0500

    adds pause and reset button to timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,29 +1,46 @@
 import React, { useEffect, useState } from "react";
 
 const Timer = ({ seconds }) => {
-  // TODO: timer functionality???
   const [secondsLeft, setSecondsLeft] = useState(seconds);
   const [timerPaused, setTimerPaused] = useState(false);
   const [timer, setTimer] = useState();
+
+  const pauseTimer = () => {
+    if (timerPaused) {
+      setTimerPaused(!timerPaused);
+      start();
+    } else {
+      return setTimerPaused(!timerPaused);
+    }
+  };
+
+  const clearTimer = () => {
+    clearInterval(timer);
+    setSecondsLeft(seconds);
+  };
+
   const start = () => {
     const timer = setInterval(() => {
+      console.log("inside setInterval");
+      setSecondsLeft((secondsLeft) => secondsLeft - 1);
+      if (secondsLeft === 0) {
+        clearInterval(timer);
+      }
+
       // if paused or not
       // if paused return seconds wihtout updating
       // if not paused keep going -1
       // pause button also checks condition if already paused
       // restart setInterval after pausing
-      setSecondsLeft((secondsLeft) => secondsLeft - 1);
-      if (secondsLeft === 0) {
-        clearInterval(timer);
-      }
     }, 1000);
     setTimer(timer);
   };
+  // whenever secondsLeft and timer re-renders/changes, useEffect will check if the seconds are 0 and clearInterval accordingly
   useEffect(() => {
-    if (secondsLeft === 0) {
+    if (secondsLeft === 0 || timerPaused) {
       clearInterval(timer);
     }
-  }, [secondsLeft, timer]);
+  }, [secondsLeft, timer, timerPaused]);
   useEffect(() => {
     return () => clearInterval(timer);
   }, [timer]);
@@ -39,6 +56,12 @@ const Timer = ({ seconds }) => {
     <div className="container">
       <button className="btn btn-primary" onClick={start}>
         Start
+      </button>
+      <button className="btn btn-primary" onClick={pauseTimer}>
+        Pause
+      </button>
+      <button className="btn btn-primary" onClick={clearTimer}>
+        Reset
       </button>
       <div>{secondsToTimeString(secondsLeft)}</div>
     </div>
